1178165ca18789acefbcf09661012486198100d6
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-04-15 18:03:44 +0200

    Création des classes

--- src/MainRechercheTabou.java
+++ src/MainRechercheTabou.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class MainRechercheTabou {
 
     public static void main(String[] args) {
+        //PRENONS N = 1
         String chemin = "src/fichiers/e_elabore.txt";
         //LectureFichier lf = new LectureFichier(args[0]);
         LectureFichier lf = new LectureFichier(chemin);
         lf.lireFichier();
         RechercheTabou rechercheTabou = new RechercheTabou();
-        System.out.println("INIT RANDOM GEN");
+        System.out.println("Debut recherche tabou");
+        ArrayList<Ingredient> listIngredients = lf.getListeIngredients();
     }
 }
--- src/RechercheTabou.java
+++ src/RechercheTabou.java
@@ -1,6 +1,33 @@
 package src;
 
+import java.util.ArrayList;
+
 public class RechercheTabou {
+    private ArrayList<Pizza> listeTabou;
+    private Pizza pizzaActuel;
     public RechercheTabou() {
+        listeTabou = new ArrayList<>();
+        pizzaActuel = new Pizza();
+    }
+    public void ajouterTabou(Pizza p){
+        listeTabou.add(p);
+    }
+    public void ajouterIngredient(Ingredient ingredient){
+        this.pizzaActuel.addIngredient(ingredient);
+    }
+    public Pizza getPizzaActuel() {
+        return pizzaActuel;
+    }
+
+    public void setPizzaActuel(Pizza pizzaActuel) {
+        this.pizzaActuel = pizzaActuel;
+    }
+
+    public void setListeTabou(ArrayList<Pizza> listeTabou) {
+        this.listeTabou = listeTabou;
+    }
+
+    public ArrayList<Pizza> getListeTabou() {
+        return listeTabou;
     }
 }
